977d6a99afc4a15414dc3fc8385cc2fa4e87e34d
Bogdansky <bogdya2@gmail.com>
2020-03-30 19:50:54 +0300

    Add enum helper

---
+++ Graduate-Work/Business Logic Layer/Helpers/EnumHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Business_Logic_Layer.Helpers
+{
+    public static class EnumHelper
+    {
+        public static string GetDescription(this Enum @enum)
+        {
+            FieldInfo fi = @enum.GetType().GetField(@enum.ToString());
+
+            DescriptionAttribute[] attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            if (attributes != null && attributes.Any())
+            {
+                return attributes.First().Description;
+            }
+
+            return @enum.ToString();
+        }
+
+        public static string GetDescriptionByValue<T>(this int value)
+        {
+            var type = typeof(T);
+            if (!type.IsEnum || !type.IsEnumDefined(value))
+            {
+                return "";
+            }
+            var enumValue = (T)Enum.ToObject(type, value) as Enum;
+            return enumValue.GetDescription();   
+        }
+    }
+}
