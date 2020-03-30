c01436191113e6a8c376f667924860a16f50ab63
Bogdansky <bogdya2@gmail.com>
2020-03-30 23:19:49 +0300

    Add filter for project search

---
+++ Graduate-Work/Business Logic Layer/Models/ProjectFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business_Logic_Layer.Models
+{
+    public class ProjectFilter
+    {
+        public string Like { get; set; }
+        public int? OrganizationId { get; set; }
+        public int? EmployeeId { get; set; }
+        public ProjectFilterEnum Filter { get; set; }
+    }
+
+    public enum ProjectFilterEnum
+    {
+        All, Mine
+    }
+}
