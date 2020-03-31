4aa804fee9717be103809ef2ba492740e8e757cb
Bogdansky <bogdya2@gmail.com>
2020-03-31 23:15:40 +0300

    Add role to employee

--- Graduate-Work/Business Logic Layer/DTO/EmployeeDTO.cs
+++ Graduate-Work/Business Logic Layer/DTO/EmployeeDTO.cs
@@ -9,6 +9,7 @@ namespace Business_Logic_Layer.DTO
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public string Patronymic { get; set; }
+        public RoleEnum Role { get; set; }
         public int? UserId { get; set; }
         public int? OrganizationId { get; set; }
         public UserDTO User { get; set; }
