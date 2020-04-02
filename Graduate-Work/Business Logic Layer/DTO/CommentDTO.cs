27b72b90c32f9d07a43ff97ea7ed070e9febb7e7
Bogdansky <bogdya2@gmail.com>
2020-04-02 23:29:30 +0300

    Add CommentDTO

---
+++ Graduate-Work/Business Logic Layer/DTO/CommentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business_Logic_Layer.DTO
+{
+    public class CommentDTO : BaseModelDTO
+    {
+        public string Description { get; set; }
+        public EmployeeDTO Author { get; set; }
+        public int? TaskId { get; set; }
+    }
+}
